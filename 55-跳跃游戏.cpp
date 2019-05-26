39afa04d94c3dd8bd599f184fe03b9904946085a
YicW <35299748+YicW@users.noreply.github.com>
2019-05-26 16:05:12 +0800

    Update 55-跳跃游戏.cpp

--- 55-跳跃游戏.cpp
+++ 55-跳跃游戏.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         auto len = nums.size();
-        int i;
         if (len <= 1)
             return true;
         auto remain_size = len - 1;//start index i.e. rbegin()
-        bool break_flag = false;
         while (1)
         {
+            int i;
             for ( i = remain_size - 1; i >= 0; i--)
             {
                 if (nums[i] >= (remain_size - i))
